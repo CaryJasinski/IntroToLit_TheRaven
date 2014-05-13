6c739b0c6fd4806e64ed8199f9c14c4e60a8ca5c
Cary Jasinski <cary.jasinski@loop.colum.edu>
2014-05-13 08:38:39 -0500

    Backup Save

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public GameObject Lightning;
+	public int Timer = 15;
+	private Light m_FlashLight;
+	private Light m_lightning;
+	public int randTimer;
+	public float randWait;
+	void Start () {
+		Transform FlashLight = transform.FindChild("FlashLight");
+		m_FlashLight = FlashLight.GetComponent<Light>();
+		m_lightning = Lightning.GetComponent<Light>();
+		StartCoroutine(LoopLightning());
+	}
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+			ToggleFlashLight();
+	}
+
+	void FixedUpdate ()
+	{
+		if(Timer > 0)
+		{
+			Timer--;
+			ToggleLightning();
+		}
+		else
+		{
+			m_lightning.enabled = false;
+		}
+	}
+	IEnumerator LoopLightning()
+	{
+		randTimer = Random.Range(10, 40);
+		Timer = randTimer;
+		randWait = Random.Range(5, 20);
+		yield return new WaitForSeconds(randWait);
+		StartCoroutine(LoopLightning());
+	}
+	void ToggleLightning()
+	{
+		if(m_lightning.enabled == true)
+			m_lightning.enabled = false;
+		else 
+			m_lightning.enabled = true;
+	}
+	void ToggleFlashLight()
+	{
+		if(m_FlashLight.enabled == true)
+			m_FlashLight.enabled = false;
+		else
+			m_FlashLight.enabled = true;
+	}
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public Texture StartMenuTexture;
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(0,0, Screen.width, Screen.height), StartMenuTexture);
+		if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 + 150, 200, 100), "StartGame"))
+		{
+			Application.LoadLevel("Game");
+		}
+	}
+}
